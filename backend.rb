fe24820dfeafa1678b27871a889cf3a51a610e98
Aditya Bhargava <bluemangroupie@gmail.com>
2013-08-18 13:59:23 -0400

    whole bunch of fixes, pages get added to db

--- backend.rb
+++ backend.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require 'mysql2'
 require 'yaml'
 
-CONFIG = Yaml.load("connection.yml").merge({'host': 'localhost', 'port': 3306})
+CONFIG = YAML.load(File.read("connection.yml")).merge({'host' => 'localhost', 'port' => 3306})
 
 def extract(url)
   begin
@@ -15,14 +15,17 @@ def extract(url)
 end
 
 def add_to_database(url, contents)
-  con = Mysql2::Client.new(CONFIG)
+  con              = Mysql2::Client.new(CONFIG)
   escaped_contents = con.escape(contents)
-  escaped_url     = con.escape(url)
-  con.query("insert into pages (url, contents) VALUES (#{escaped_url}, #{escaped_contents})")
+  escaped_url      = con.escape(url)
+  query            = "insert into pages (url, contents) VALUES ('#{escaped_url}', '#{escaped_contents}')"
+  con.query query
   con.close
 end
 
-get '/add/:url' do
-  contents = extract(params[:url])
-  add_to_database(contents)
+get '/add' do
+  url      = params[:url]
+  contents = extract url
+  add_to_database(url, contents)
+  "#{url} added to db!"
 end
--- migrations.rb
+++ migrations.rb
@@ -2,14 +2,14 @@ require 'rubygems'
 require 'mysql2'
 require 'yaml'
 
-CONFIG = Yaml.load("connection.yml").merge({'host': 'localhost', 'port': 3306})
+CONFIG = YAML.load(File.read("connection.yml")).merge({'host' => 'localhost', 'port' => 3306})
 
 con = Mysql2::Client.new(CONFIG)
 
 con.query(%{
 CREATE TABLE IF NOT EXISTS pages (
   id integer PRIMARY KEY AUTO_INCREMENT,
-  url STRING,
+  url VARCHAR(2048),
   contents TEXT
   );
 })
